71ddf8471b6d676772ff8ff5c2ddcb81791e4b7e
Laraib <laraibfakhar@gmail.com>
2023-04-05 18:29:04 +0200

    Let Array code

---
+++ JSAutomation/Array.js
@@ -0,0 +1,8 @@
+var names=["Fatima", "Zainab", "Ali", "Ahmed"]
+var score=["90","97","65","69","86"]
+
+for(let n of names){
+    for(let s of score){
+        console.log("Student "+n+ " has scored "+s)
+    }
+}
