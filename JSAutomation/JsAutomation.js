5eec8f3fc0d59bc03e6ae02cd569c1c9d27e2332
Laraib <laraibfakhar@gmail.com>
2023-04-05 18:01:16 +0200

    Deployment JS code

---
+++ JSAutomation/JsAutomation.js
@@ -0,0 +1,9 @@
+var joblocation="EU"
+var yearsofExperience=8
+//eligible for job if the years of experience in between 6-9
+
+if((yearsofExperience>=6 && yearsofExperience<=9) && (jobtype="EU")){
+console.log("the candidate is eligible")
+}else{
+    console.log("the candidate is not eligible")
+}
